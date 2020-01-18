29a8e4f95a7e5807033ba6913e1fb9b7c40a7ef7
Diksu <Szymek_diks@interia.pl>
2020-01-18 20:16:48 +0100

    Minor changes to run

--- core/app.py
+++ core/app.py
@@ -4,8 +4,12 @@ from sklearn.cluster import KMeans, DBSCAN, AffinityPropagation
 
 class Core:
 
+    def __init__(self):
+        self.data = None
+        self.pred = "pred"
+
     def cluster(self, method):
-        data = pd.read_csv("data/bank-full.csv")
+        data = pd.read_csv("data/bank-processed.csv")
 
         if method == "KMeans":
             model = KMeans(n_clusters=3)
@@ -15,12 +19,13 @@ class Core:
             model = AffinityPropagation(n_cluster=3)
 
         predictions = model.fit_predict(data)
-        data["pred"] = predictions.tolist()
+        data[self.pred] = predictions.tolist()
+        self.data = data
 
     def get_results(self, xaxis, yaxis):
-        data_to_present = pd.DataFrame(columns=['x','y','label'])
-        data_to_present["x"] = data[xaxis]
-        data_to_present["y"] = data[yaxis]
-        data_to_present["label"] = data[pred]
+        data_to_present = pd.DataFrame(columns=['x', 'y', 'label'])
+        data_to_present["x"] = self.data[xaxis]
+        data_to_present["y"] = self.data[yaxis]
+        data_to_present["label"] = self.data[self.pred]
 
         return data_to_present

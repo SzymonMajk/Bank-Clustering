d92ffe48505594063ffa406bb6cbd9bee32d07b7
Diksu <Szymek_diks@interia.pl>
2020-01-18 19:12:01 +0100

    visualization with stub results

--- core/app.py
+++ core/app.py
@@ -1,8 +1,30 @@
+import pandas as pd
+import numpy as np
+
+
 class Core:
 
-	def cluster(method):
-		pass
+    def cluster(self, method):
+        pass
 
-	"""Desired format: array of tuples"""
-	def get_results(xaxis, yaxis):
-		pass
+    def get_results(self, xaxis, yaxis):
+
+        #number of points per group
+        n = 50
+
+        #define group labels and their centers
+        groups = {'A': (2,2),
+            'B': (3,4),
+            'C': (4,4),
+            'D': (4,1)}
+
+        #create labeled x and y data
+        data = pd.DataFrame(index=range(n*len(groups)), columns=['x','y','label'])
+        for i, group in enumerate(groups.keys()):
+            #randomly select n datapoints from a gaussian distrbution
+            data.loc[i*n:((i+1)*n)-1,['x','y']] = np.random.normal(groups[group], 
+                                                           [0.5,0.5], 
+                                                           [n,2])
+            #add group labels
+            data.loc[i*n:((i+1)*n)-1,['label']] = group
+        return data
---
+++ run.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from argparse import ArgumentParser
+from core.app import Core
+
+
+methods = ('kmeans', 'rforest', 'dtree')
+features = ('age', 'job', 'martial', 'education', 'default', 'balance', 'housing', 'loan', 'duration')
+plt.rc('font', size=16)
+
+#set style of plots
+sns.set_style('white')
+
+#define a custom palette
+customPalette = ['#630C3A', '#39C8C6', '#D3500C', '#FFB139']
+sns.set_palette(customPalette)
+sns.palplot(customPalette)
+
+df = st.cache(pd.read_csv)("data/bank-full.csv")
+st.title("Bank clustering")
+is_check = st.sidebar.checkbox("Display Data")
+
+method = st.sidebar.radio('Which cluster method?', methods)
+xaxis = st.sidebar.radio('Feature on x axis', features)
+yaxis = st.sidebar.radio('Feature on y axis', features)
+
+cluster = st.sidebar.button("Cluster!")
+
+if is_check:
+	st.write(df)
+
+if cluster:
+	core = Core()
+	core.cluster(method)
+	results = core.get_results(xaxis, yaxis)
+
+	#plot data with seaborn
+	facet = sns.lmplot(data=results, x='x', y='y', hue='label', fit_reg=False, legend=True, legend_out=True)
+	st.pyplot()
